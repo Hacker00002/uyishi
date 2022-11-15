0838468d651d3a03842a5d4d84412512fa80582e
Hacker00002 <abdulbositsharipovv@gmail.com>
2022-11-15 21:36:59 +0500

    kk

---
+++ script.js
@@ -0,0 +1,28 @@
+let userName = prompt("Ismingizni kiriting");
+let userAge = prompt("Yoshingiz nechida")
+let userAbout = prompt("Qayerda Tugilgansiz")
+let userSchool = prompt("Qaysi maktabda o'qigansiz")
+let userAboutTwo = prompt("Qaysi to'garaklarga borgansiz")
+let userAll = alert("Bizda sizning barcha malumotlaringiz bor,Savollarga javob berganingiz uchun tashakkur :)")
+// console.log(userName);
+
+let allName = alert(`${userName}\n${userAge}\n${userAbout}\n${userSchool}\n${userAboutTwo}`)
+
+console.log(userAll);
+console.log(`Mening ismim ${userName}, Yoshim ${userAge} da, Men ${userAbout} Shahrida Tugilganman,  ${userSchool} maktabida o'qiganman, ${userAboutTwo} to'garagida Oqiganman`);
+
+
+let a = 25
+let b = 15
+
+console.log(a-b);
+
+let d = 10
+let c =5
+let g = d-c
+
+let all = alert(`${a-b} \n ${g}`)
+
+console.log(g);
+
+// alert(60 * 60 * 24);
